ac0b7970ba603d297ddc7a9f4283ca4e861313b1
eitanamzallag <eitanamzallag@gmail.com>
2026-08-10 00:10:33 +0300

    Initial commit

---
+++ protocol/packet.py
@@ -0,0 +1,19 @@
+from dataclasses import dataclass
+from enum import IntEnum
+
+class MessageType(IntEnum):
+    TEXT = 0x01
+    PING = 0x02
+    PONG = 0x03
+    ACK = 0x04
+    ERROR = 0x05
+
+class MessageFlags(IntEnum):
+    ACK_REQUIRED = 0x01
+
+@dataclass
+class Packet:
+    packet_type: MessageType
+    message_id: int
+    payload: bytes
+    flags: int = 0
